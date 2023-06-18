3ec174cba7d2fe368e4734bbdadd99ffd8084131
OzLievano <Osvaldoalievano@gmail.com>
2023-06-17 23:34:03 -0400

    updated test string

--- src/checkPermutation.test.ts
+++ src/checkPermutation.test.ts
@@ -1,14 +1,14 @@
 import {checkPermutations} from './checkPermutation';
 
 
-test('isUnique', () => {
+test('checkPermutations', () => {
   const stringToTest1 = 'abracadabra';
   const stringToTest2 = 'abracadabras';
   expect(checkPermutations(stringToTest1,stringToTest2)).toBe(false);
 } )
 
 
-test('isUnique', () => {
+test('checkPermutations', () => {
   const stringToTest1 = 'Barn';
   const stringToTest2 = 'narb';
   expect(checkPermutations(stringToTest1, stringToTest2)).toBe(true);
